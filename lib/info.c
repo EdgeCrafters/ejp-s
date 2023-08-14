485f6f9d04a827aaccc34251ae67fdbe5f0819db
Hanjunhee-1 <hanjuni857@naver.com>
2023-08-14 11:31:57 +0900

    chore: 안내문구 수정

--- lib/info.c
+++ lib/info.c
@@ -24,8 +24,8 @@ void problemInfo(void)
 {
 	basicInfo();
 	printf("problem command : \n\n ");
-	printf("1. get: Receive questions written by your instructor.\n\n ");
-	printf("2. show: Read all questions written by your instructor. \n\n ");
+	printf("1. get: Receive problems written by your instructor.\n\n ");
+	printf("2. show: Read all problems written by your instructor. \n\n ");
 	printf("3. submit: Submit your answer(maybe..?) code.\n\n ");
 	printf("4. test: Test your code with testcase published by your instructor. \n\n");
 	printf(":) \n\n");
